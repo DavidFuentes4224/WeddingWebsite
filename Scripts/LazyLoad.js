14822a3a53af5d4b7c7b56eea00bbd034d998e6a
David <davidfuentes4224@yahoo.com>
2019-12-24 13:28:17 -0800

    Lazy Load Support
    * Added lazy loading and resolution restrictions so to images
    * Fixed hotels.com link

--- Scripts/LazyLoad.js
+++ Scripts/LazyLoad.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-      
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-              }
-          });
-          if(lazyloadImages.length == 0) { 
-            document.removeEventListener("scroll", lazyload);
-            window.removeEventListener("resize", lazyload);
-            window.removeEventListener("orientationChange", lazyload);
-          }
-      }, 20);
-    }
-    
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  });
+const lazyloadImages = document.querySelectorAll("[data-src]");    
+
+function preloadImage(img) {
+  const src= img.getAttribute("data-src");
+  if(!src)
+    return;
+  
+    img.src = src;
+}
+
+const imgOptions = {
+};
+
+const imgObserver = new IntersectionObserver(
+  (entries, imgObserver) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+      }
+    })
+  },
+  imgOptions);
+
+lazyloadImages.forEach(image => {
+  imgObserver.observe(image);
+});
