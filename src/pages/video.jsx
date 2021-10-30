b1bcb7628763eb16b1712ee0eeb4f2e61bd85292
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2021-10-30 12:29:34 -0700

    added video

--- src/pages/video.jsx
+++ src/pages/video.jsx
@@ -1,7 +1,13 @@
 export const Video = () =>{
     return (
         <>
-        <h1>Coming Soon</h1>
+         <div className='main-page'>
+         <div className="centered" style={{padding:'50px'}}>
+         <iframe width="765" height="500" src="https://www.youtube.com/embed/4Ynx6GwYz5w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+             </div>
+
+         </div>
         </>
     )
 }
